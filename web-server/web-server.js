aea32db2700be9c692816bbb11261c6e94ab5f02
NikitaMikheev <nikita.miheev.1999@mail.ru>
2023-02-16 15:28:17 +0300

    server

--- web-server/web-server.js
+++ web-server/web-server.js
@@ -5,6 +5,7 @@ const port = 8000;
 
 const requestListener = function (req, res) {
     res.writeHead(200);
+    req.write('Hello');
     res.end('Первый сервер');
 }
 
@@ -12,3 +13,10 @@ const server = http.createServer(requestListener);
 server.listen(port, host, () => {
     console.log(`Сервер по адресу http://${host}:${port}`);
 })
+
+async function zapros() { // попытка запроса на сервер
+  let response = await fetch(); // ТУТ УКАЗАТЬ URL !!!
+  console.log(response);
+}
+
+zapros();
