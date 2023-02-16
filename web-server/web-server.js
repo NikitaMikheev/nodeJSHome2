35d46b18caa4be3f3d81073768cd8012b3954ad5
Nikita Mikheev <nikita.miheev.1999@mail.ru>
2023-02-16 20:41:59 +0300

    сервер работает, возвращает html

--- web-server/web-server.js
+++ web-server/web-server.js
@@ -1,12 +1,16 @@
-const http = require("http");
+import http from 'http';
+import {readFile} from 'fs/promises';
 
 const host = 'localhost';
 const port = 8000;
 
 const requestListener = function (req, res) {
-    res.writeHead(200);
-    req.write('Hello');
-    res.end('Первый сервер');
+    readFile("C:/Users/Никита/Desktop/nodeJsHomework2/nodeJSHome2/web-server/index.html")
+    .then(contents => {
+      res.setHeader("Content-Type", "text/html");
+      res.writeHead(200);
+      res.end(contents);
+    })
 }
 
 const server = http.createServer(requestListener);
@@ -14,9 +18,10 @@ server.listen(port, host, () => {
     console.log(`Сервер по адресу http://${host}:${port}`);
 })
 
-async function zapros() { // попытка запроса на сервер
-  let response = await fetch(); // ТУТ УКАЗАТЬ URL !!!
-  console.log(response);
-}
+// async function zapros() { // попытка запроса на сервер. Запрос сработал!
+//   let response = await fetch('http://localhost:8000'); 
+//   let text = await response.text();
+//   console.log(text);
+// }
 
-zapros();
+// zapros();
